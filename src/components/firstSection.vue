6953518498f607bd01e8171b61b8bd7af7b7084d
Tomas Diaco <tomas.diaco@avantrip.com>
2018-10-11 12:50:39 -0300

    pug added

---
+++ src/components/firstSection.vue
@@ -0,0 +1,19 @@
+<template lang='pug'>
+  section
+   | holaaa
+</template>
+
+<script>
+export default {
+  name: 'firstSection',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
